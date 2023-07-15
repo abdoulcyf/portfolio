e8bd37603f86d947299587c1cf29c907071f1bd7
abdoulcyf <138031973+abdoulcyf@users.noreply.github.com>
2023-07-15 20:41:31 +0100

    Add files via upload

---
+++ Scrimab - CSS Variables/first variable/index.js
@@ -0,0 +1,15 @@
+// var root = document.querySelector(":root");
+// var rootStyle = getComputedStyle(root);
+// var red = rootStyle.getPropertyValue("--red");
+// console.log("red:", red);
+
+// root.style.setProperty("--red", "green");
+
+// var root = document.querySelector(":root");
+
+// var rootStyle = getComputedStyle(root);
+
+// var yellow = rootStyle.getPropertyValue("--yellow");
+// console.log("yellow:", yellow);
+
+// root.style.setProperty("--yellow", "black");
